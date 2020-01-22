a805b24f7482808383a7e51add223383b04b21de
LZF <lzf19961231@sina.com>
2020-01-22 17:33:09 +0800

    还在搞首页

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -1,26 +1,48 @@
 <template>
-  <div id="home">
-    <navBar class="home-nav">
-      <div  slot="center">购物街</div>
-    </navBar>
-  </div>
+    <div id="home">
+        <navBar class="home-nav">
+            <div slot="center">购物街</div>
+        </navBar>
+        <homeSwiper :banners="banners"/>
+        <recommendView :recommends="recommends"/>
+    </div>
 </template>
 
 <script>
-  import navBar from 'components/common/navBar/navBar';
-  import {getHomeMultiData} from "network/home";
+    import navBar from 'components/common/navBar/navBar';
+    import homeSwiper from './childComps/homeSwiper';
+    import recommendView from './childComps/recommendView'
 
-  export default {
-    name: "home",
-    components: {
-      navBar
+    import {getHomeMultiData} from "network/home";
+
+
+    export default {
+        name: "home",
+        components: {
+            navBar,
+            homeSwiper,
+            recommendView
+        },
+        data() {
+            return {
+                banners: [],
+                recommends: []
+            }
+        },
+        created() {
+            getHomeMultiData().then(res => {
+                this.result = res;
+                this.banners = res.data.banner.list;
+                this.recommends = res.data.recommend.list;
+                console.log(this.$data);
+            });
+        }
     }
-  }
 </script>
 
 <style scoped>
-  .home-nav{
-    background-color: var(--color-tint);
-    color: #fff;
-  }
+    .home-nav {
+        background-color: var(--color-tint);
+        color: #fff;
+    }
 </style>
